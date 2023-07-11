d8eb5714f3b087bbb445f2ef22ebe62805a9a7f0
tamson314159 <tamson314159@gmail.com>
2023-07-11 18:01:16 +0900

    クローラー Fixes #76

---
+++ simple_crawler/lib/simple_crawler/application.ex
@@ -0,0 +1,20 @@
+defmodule SimpleCrawler.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: SimpleCrawler.Worker.start_link(arg)
+      # {SimpleCrawler.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: SimpleCrawler.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
