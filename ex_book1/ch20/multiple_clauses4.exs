281a3796243e9d046bdc26b88e6da1d23cbfc9cf
tamson314159 <tamson314159@gmail.com>
2023-07-25 12:24:47 +0900

    Elixir 実践ガイド 第20章 練習問題 Fixes #178

---
+++ ex_book1/ch20/multiple_clauses4.exs
@@ -0,0 +1,17 @@
+defmodule MyMod do
+  def parse("* " <> name), do: %{name: name, checked: true}
+  def parse("  " <> name), do: %{name: name, checked: false}
+  def parse(""), do: nil
+end
+
+{:ok, data} = File.read("names.txt")
+
+members =
+  data
+  |> String.trim_trailing()
+  |> String.split("\n")
+  |> Enum.map(fn line -> MyMod.parse(line) end)
+  # |> Enum.filter(&!is_nil(&1))
+  |> Enum.reject(&is_nil/1)
+
+IO.inspect members
